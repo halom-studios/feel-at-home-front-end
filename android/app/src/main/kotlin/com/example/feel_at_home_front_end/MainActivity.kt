52ceadce06cb39e1ce54db5e935507a499d20cfc
Michel Martinez <michelsm1990@gmail.com>
2021-08-31 11:02:14 -0300

    feat: commit inicial

---
+++ android/app/src/main/kotlin/com/example/feel_at_home_front_end/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feel_at_home_front_end
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Feel at home',
+      theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                Colors.blue,
+              ), //button color
+              foregroundColor: MaterialStateProperty.all<Color>(
+                Colors.white,
+              ), //text (and icon)
+            ),
+          ),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.yellow,
+            textTheme: ButtonTextTheme.primary,
+          )),
+      home: MyHomePage(title: 'Feel at home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    _passwordVisible = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.05),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.login),
+                      title: Text('Entrar na aplicação'),
+                      subtitle: Text('Caso não tenha usuário entre em contato'),
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: 'E-mail',
+                              ),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Por favor digite um email válido';
+                                }
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              obscureText: !_passwordVisible,
+                              decoration: InputDecoration(
+                                // filled: true,
+                                hintText: "Senha",
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _passwordVisible = !_passwordVisible;
+                                    });
+                                  },
+                                  icon: Icon(_passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 16.0),
+                              child: Row(children: [
+                                Spacer(),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      // TODO: validar dados e fazer o login
+                                    }
+                                  },
+                                  child: const Text('Submit'),
+                                ),
+                              ]),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
